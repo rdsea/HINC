1b4abdcb9f61d8a19516bbca3f627549f9a26b37
Le Duc Hung <d.le@dsg.tuwien.ac.at>
2016-03-16 16:50:00 +0100

    move from SALSA to the new repo

---
+++ model/src/main/java/at/ac/tuwien/dsg/cloud/salsa/model/VirtualComputingResource/Capability/CapabilityType.java
@@ -0,0 +1,8 @@
+package at.ac.tuwien.dsg.cloud.salsa.model.VirtualComputingResource.Capability;
+
+public enum CapabilityType {
+    DataPoint,
+    ControlPoint,    
+    ExecutionEnvironment,
+    CloudConnectivity
+}
