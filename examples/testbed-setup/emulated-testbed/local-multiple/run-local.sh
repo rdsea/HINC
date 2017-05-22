c6c6fa816d1dd3630d514cfaf8653705b4971673
Le Duc Hung <d.le@dsg.tuwien.ac.at>
2017-05-22 02:59:26 +0200

    testbed scripts

--- examples/testbed-setup/emulated-testbed/local-multiple/run-local.sh
+++ examples/testbed-setup/emulated-testbed/local-multiple/run-local.sh
@@ -1,7 +1,26 @@
-GW_START=$1
-GW_END=$2
+#!/bin/bash
 
-for i in "$@"
+# Option 1: Set parameter below, and run: ./run-local.sh
+# - Dummy provider generate sensors metadata (1 instances)
+# - Several HINC local is run (many instances)
+#
+# Option 2: Run: ./run-local.sh GATEWAYS_NUM SENSORS_NUM
+# For example: ./run-local.sh 5 50  (5 gateways, each simulate 50 sensors)
+
+NUMBER_OF_GATEWAYS=5
+NUMBER_OF_SENSORS=50
+
+if [ $# -eq 2 ]; then
+  NUMBER_OF_GATEWAYS=$1
+  NUMBER_OF_SENSORS=$2
+fi
+mkdir -p localrun
+
+# start dummy provider
+nohup ./run-dummy.sh $NUMBER_OF_SENSORS &
+
+# start HINC Local
+for ((i=1; i<=NUMBER_OF_GATEWAYS; i++))
 do
   path=localrun/local$i
   echo "running in $path"
