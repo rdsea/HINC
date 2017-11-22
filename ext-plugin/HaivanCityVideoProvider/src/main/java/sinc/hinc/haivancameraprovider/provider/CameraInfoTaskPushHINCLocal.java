c695d620a26bfa74ee474f37061b4a21ffd3a023
Hong-Linh Truong <linhsolar@gmail.com>
2017-11-22 15:46:59 +0100

    update readme and example

--- ext-plugin/HaivanCityVideoProvider/src/main/java/sinc/hinc/haivancameraprovider/provider/CameraInfoTaskPushHINCLocal.java
+++ ext-plugin/HaivanCityVideoProvider/src/main/java/sinc/hinc/haivancameraprovider/provider/CameraInfoTaskPushHINCLocal.java
@@ -18,6 +18,7 @@ import sinc.hinc.communication.processing.HINCMessageHander;
 import sinc.hinc.communication.processing.HINCMessageSender;
 import sinc.hinc.communication.processing.HincMessage;
 import sinc.hinc.model.API.WrapperIoTUnit;
+import sinc.hinc.model.VirtualComputingResource.IoTUnit;
 
 /**
  *
@@ -25,27 +26,31 @@ import sinc.hinc.model.API.WrapperIoTUnit;
  */
 public class CameraInfoTaskPushHINCLocal {
 
-    static Logger logger = LoggerFactory.getLogger("HaivanCamera");    
+    static Logger logger = LoggerFactory.getLogger("HaivanCamera");
     List<CameraMetadataItem> dataItems;
     Map<String, String> meta = null;
     String amqpURL;
+
     public CameraInfoTaskPushHINCLocal(String amqpURL, List<CameraMetadataItem> dataItemsToSend, Map<String, String> meta) {
         this.dataItems = dataItemsToSend;
         this.meta = meta;
-        this.amqpURL =amqpURL;
+        this.amqpURL = amqpURL;
     }
-     
+
     public void push() {
         CameraTransformer transformer = new CameraTransformer();
         WrapperIoTUnit wrapper = new WrapperIoTUnit();
         for (CameraMetadataItem data : dataItems) {
             wrapper.getUnits().add(transformer.translateIoTUnit(data));
         }
-
+        String json = wrapper.toJson();
+        logger.debug(json);
         HINCMessageSender comMng = new HINCMessageSender(amqpURL, "amqp");
-        HincMessage msg = new HincMessage(HINCMessageType.PROVIDER_UPDATE_IOT_UNIT.toString(), "HaivanCityVideoProvider", HincMessageTopic.getBroadCastTopic(""), "", wrapper.toJson());
-        msg.hasExtra("test", "experiment");
+        HincMessage msg = new HincMessage(HINCMessageType.PROVIDER_UPDATE_IOT_UNIT.toString(), "HaivanCityVideoProvider", HincMessageTopic.getBroadCastTopic("haivancamera"), "", json);
+        msg.hasExtra("haivancamera", "experiment");
         msg.getExtra().putAll(meta);
+        comMng.synCall(msg);
+        /*
         comMng.asynCall(0, msg, new HINCMessageHander() {
             @Override
             public HincMessage handleMessage(HincMessage message) {
@@ -53,29 +58,45 @@ public class CameraInfoTaskPushHINCLocal {
                 return null;
             }
         });
+        */
     }
 
-    public static void main(String args[]) {
+    public void test() {
+        HincMessage request = new HincMessage()
+                .hasType(HINCMessageType.QUERY_INFORMATION_GLOBAL)
+                .hasSenderID("my-application")
+                .hasTopic(HincMessageTopic.getBroadCastTopic("haivancamera"));
+        HINCMessageSender sender = new HINCMessageSender(amqpURL, "amqp");
+        String responseMsg = sender.synCall(request);
+        logger.debug(responseMsg);
+
+    }
+
+    public static void main(String args[]) throws InterruptedException {
         CameraIoTDevices allData = new CameraIoTDevices();
         List<CameraMetadataItem> dataItemsToSend = new ArrayList<>();
-        CameraMetadataItem item1 =new CameraMetadataItem();
+        CameraMetadataItem item1 = new CameraMetadataItem();
         item1.setId("id1");
         item1.setDescription("camera 1");
         item1.setName("linh");
         item1.setUrl("http://2co2.vp9.tv/chn/DNG33");
         dataItemsToSend.add(item1);
-        CameraMetadataItem item2 =new CameraMetadataItem();
+        CameraMetadataItem item2 = new CameraMetadataItem();
         item2.setId("id2");
         item2.setDescription("camera 2");
         item2.setName("vu");
         item2.setUrl("http://2co2.vp9.tv/chn/DNG2");
         dataItemsToSend.add(item2);
-        
+
         Map<String, String> meta = new HashMap<>();
         Long st_device_change = new Date().getTime();
         meta.put("st_device_change", st_device_change + "");
         CameraInfoTaskPushHINCLocal task;
-        task = new CameraInfoTaskPushHINCLocal(args[1],dataItemsToSend, meta);
-        task.push();
+        task = new CameraInfoTaskPushHINCLocal(args[1], dataItemsToSend, meta);
+        while (true) {
+            task.push();
+            Thread.sleep(10000);
+            task.test();
+        }
     }
 }
