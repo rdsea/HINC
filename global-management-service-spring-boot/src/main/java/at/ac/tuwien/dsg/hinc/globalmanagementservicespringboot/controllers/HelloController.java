c6d489ceb41ecd6ec5a598615b3799da5d05fdf0
Michael Hammerer <m.hammerer27@gmail.com>
2018-04-09 11:37:55 +0200

    added swagger support and testcontroller

---
+++ global-management-service-spring-boot/src/main/java/at/ac/tuwien/dsg/hinc/globalmanagementservicespringboot/controllers/HelloController.java
@@ -0,0 +1,28 @@
+package at.ac.tuwien.dsg.hinc.globalmanagementservicespringboot.controllers;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/test")
+public class HelloController {
+
+    @GetMapping("/")
+    public ResponseEntity index() {
+        return new ResponseEntity("Greetings from Spring Boot HINC!", HttpStatus.OK);
+    }
+
+
+    @GetMapping("/greeting")
+    public ResponseEntity greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return new ResponseEntity("greeting", HttpStatus.OK);
+    }
+
+
+}
