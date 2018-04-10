89f578cd52e45fb7a7640fe9d1c579c3e2a4f1f5
ling <lingfangao@gmail.com>
2018-04-10 17:15:10 +0200

    added more commands to pizza

--- slice-management-client/src/cmds/provider_cmds/list.js
+++ slice-management-client/src/cmds/provider_cmds/list.js
@@ -1,4 +1,5 @@
 const amqpTools = require('../../amqpTools');
+const db = require('../../data/db');
 
 exports.command = 'list'
 exports.desc = 'list available resource providers'
@@ -7,6 +8,12 @@ exports.builder = {
         alias: 'l',
         describe: 'limit the number of results',
         type: 'number'
+    },
+    refresh:{
+        alias: 'r',
+        describe: 'refresh data from datasource',
+        type: 'boolean',
+        demandOption: false
     }
 }
 
@@ -14,22 +21,50 @@ exports.handler = function (argv) {
     let payload = argv.limit ? { limit: argv.limit } : '';
     let query = amqpTools.buildMessage('FETCH_PROVIDERS', payload);
 
-    return amqpTools.init().then(() => {
-        return amqpTools.sendMessage(query);
-    }).then(() => {
-        return amqpTools.getMessage();
-    }).then((msg) => {
-        msg = JSON.parse(msg.content.toString());
-        if(msg.msgType !== 'DELIVER_PROVIDERS')
-            throw new Error(`unexpected message reply received, expected DELIVER_PROVIDERS but got ${msg.msgType}`);
-        let providers = JSON.parse(msg.payload);
-        providers.forEach((resource, count) => {
-            console.log(JSON.stringify(resource, null, 2));
-            console.log('\n================================================\n')
-        });    
+    if(!(argv.refresh)){
+        return db.providerDao().find({}).then((resources) => {
+            _displayProviders(resources);
+        });
+    }else{
+        return amqpTools.init().then(() => {
+            return amqpTools.sendMessage(query);
+        }).then(() => {
+            return amqpTools.getMessage();
+        }).then((msg) => {
+            msg = JSON.parse(msg.content.toString());
+            if(msg.msgType !== 'DELIVER_PROVIDERS')
+                throw new Error(`unexpected message reply received, expected DELIVER_PROVIDERS but got ${msg.msgType}`);
+            let providers = JSON.parse(msg.payload);
+            return _refreshProviders(providers)
+            console.log(`retrieved ${providers.length} providers`)
+        }).then((providers) => {
+            _displayProviders(providers)
+        }).catch((err) => {
+            console.err(err);
+        }).finally(() => amqpTools.close());
+    }
 
-        console.log(`retrieved ${providers.length} providers`)
-    }).catch((err) => {
-        console.err(err);
-    }).finally(() => amqpTools.close());
+    
+}
+
+function _refreshProviders(providers){
+    return db.providerDao().remove({}, {multi: true}).then(() => {
+        let promises = [];
+        providers.forEach((provider) => {
+        promises.push(db.providerDao().insert(provider));
+    });
+        return Promise.all(promises);
+    }).then(() => {
+        return providers;
+    });    
+}
+
+
+
+function _displayProviders(providers){
+    providers.forEach((provider, count) => {
+        console.log(JSON.stringify(provider, null, 2));
+        console.log('\n================================================\n')
+    });    
+    console.log(`retrieved ${providers.length} providers`)
 }
--- slice-management-client/src/config.js
+++ slice-management-client/src/config.js
@@ -2,4 +2,4 @@ exports.uri = 'amqp://guest:guest@localhost';
 exports.exchange = 'test.adaptors';
 exports.routingKey = 'pizza';
 exports.localRoutingKey = 'test.local';
-exports.production = true;
+//exports.production = true;
