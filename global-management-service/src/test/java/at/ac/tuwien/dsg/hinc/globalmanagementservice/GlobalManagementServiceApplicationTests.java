a04b0c7298dc340ab8a3cb7fa7f1870a952c62a7
Michael Hammerer <m.hammerer27@gmail.com>
2018-06-13 11:19:22 +0200

    refactored application class of globalmanagementservice from GlobalManagementServiceSpringBootApplication to GlobalManagementServiceApplication

--- global-management-service/src/test/java/at/ac/tuwien/dsg/hinc/globalmanagementservice/GlobalManagementServiceApplicationTests.java
+++ global-management-service/src/test/java/at/ac/tuwien/dsg/hinc/globalmanagementservice/GlobalManagementServiceApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class GlobalManagementServiceSpringBootApplicationTests {
+public class GlobalManagementServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
