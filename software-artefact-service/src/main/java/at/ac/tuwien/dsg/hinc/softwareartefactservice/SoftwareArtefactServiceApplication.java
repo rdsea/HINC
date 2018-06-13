2a2467f5a7629d59b62585b3a45e4ecb5e959aab
Michael Hammerer <m.hammerer27@gmail.com>
2018-06-13 11:08:36 +0200

    added software-artefact-service module with spring.initializr

---
+++ software-artefact-service/src/main/java/at/ac/tuwien/dsg/hinc/softwareartefactservice/SoftwareArtefactServiceApplication.java
@@ -0,0 +1,12 @@
+package at.ac.tuwien.dsg.hinc.softwareartefactservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareArtefactServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftwareArtefactServiceApplication.class, args);
+    }
+}
---
+++ software-artefact-service/src/test/java/at/ac/tuwien/dsg/hinc/softwareartefactservice/SoftwareArtefactServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package at.ac.tuwien.dsg.hinc.softwareartefactservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SoftwareArtefactServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
