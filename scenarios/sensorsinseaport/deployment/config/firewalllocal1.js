f1cdc16e43705feec820bb5dbf28075d18975431
ling <lingfangao@gmail.com>
2018-07-24 11:44:16 +0800

    README

---
+++ scenarios/sensorsinseaport/deployment/config/firewalllocal1.js
@@ -0,0 +1,6 @@
+module.exports = {
+        ADAPTOR_NAME : 'firewalllocal1',
+        URI : 'amqp://kikghwrj:VBze-MjoklEt7NSpPlSWSEUSJwxJOoCW@sheep.rmq.cloudamqp.com/kikghwrj',
+        EXCHANGE : 'adaptor_local1_input',
+        ENDPOINT: 'http://firewallproviderlocal1:3008'
+    }
---
+++ scenarios/sensorsinseaport/deployment/config/ingestionlocal1.js
@@ -0,0 +1,6 @@
+module.exports = {
+        ADAPTOR_NAME : 'ingestionlocal1',
+        URI : 'amqp://kikghwrj:VBze-MjoklEt7NSpPlSWSEUSJwxJOoCW@sheep.rmq.cloudamqp.com/kikghwrj',
+        EXCHANGE : 'adaptor_local1_input',
+        ENDPOINT: 'http://ingestionproviderlocal1:3003'
+    }
