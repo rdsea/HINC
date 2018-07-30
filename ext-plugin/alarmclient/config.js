dcbbf8246946c168002b76491a29d60048e40965
Hong-Linh Truong <linhsolar@gmail.com>
2018-07-30 15:22:56 +0200

    update the document of the alarm plugin

--- ext-plugin/alarmclient/config.js
+++ ext-plugin/alarmclient/config.js
@@ -1,6 +1,8 @@
 module.exports = {
-    ADAPTOR_NAME : 'alarmclient',
+    //name of this plugin
+    ADAPTOR_NAME : 'seaportalarmplugin',
     URI : 'amqp://zldxlzzt:feJDiwOkdkVuzuqwIXohBG0_hs4VOFVA@sheep.rmq.cloudamqp.com/zldxlzzt',
     EXCHANGE : 'adaptor_local_input',
+    //endpoint of the Alarm Provider
     ENDPOINT: 'http://localhost:3006'
 }
