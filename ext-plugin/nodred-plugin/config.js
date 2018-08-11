3c67347607ed05305b3636f58d64ea79661be3f1
ling <lingfangao@gmail.com>
2018-08-11 17:17:13 +0200

    nodered adaptor

---
+++ ext-plugin/nodred-plugin/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    ADAPTOR_NAME : 'nodered',
+    URI : 'amqp://zldxlzzt:feJDiwOkdkVuzuqwIXohBG0_hs4VOFVA@sheep.rmq.cloudamqp.com/zldxlzzt',
+    EXCHANGE : 'adaptor_local_input',
+    ENDPOINT: 'http://localhost:3004'
+}
+
+
---
+++ ext-plugin/nodred-plugin/src/messageHandlers/handler.js
@@ -0,0 +1,34 @@
+class Handler{
+    constructor(){
+        this.handlers = [];
+    }
+
+    use(msgType ,handle){
+        this.handlers.push({
+            msgType,
+            handle,
+        });
+    }
+
+    handle(msg){
+        for(let i=0;i<this.handlers.length;i++){
+            if(msg.msgType !== this.handlers[i].msgType) continue;
+            return this.handlers[i].handle(msg);
+        }
+        console.log(`no handler found for ${msg.msgType}`);
+        return new Promise((resolve, reject) => resolve(null))
+    }
+}
+
+let handler = new Handler();
+// register handlers
+handler.use('QUERY_RESOURCES', require('./handleQueryResources'));
+handler.use('QUERY_PROVIDER', require('./handleQueryProvider'));
+handler.use('CONTROL', require('./handleSendControl'));
+handler.use('PROVISION', require('./handleProvision'));
+handler.use('DELETE', require('./handleDelete'));
+handler.use('CONFIGURE', require('./handleConfigure'));
+handler.use('GET_LOGS', require('./handleGetLogs'));
+
+module.exports = handler
+
