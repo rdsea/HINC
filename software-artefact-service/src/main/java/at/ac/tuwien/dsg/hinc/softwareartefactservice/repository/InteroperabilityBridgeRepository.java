cf4e97e12039afcc018296a0b7aa6b8df96bc35f
Michael Hammerer <m.hammerer27@gmail.com>
2018-09-13 10:48:02 +0200

    fixed package name of repository in software-artefact-service

--- software-artefact-service/src/main/java/at/ac/tuwien/dsg/hinc/softwareartefactservice/repository/InteroperabilityBridgeRepository.java
+++ software-artefact-service/src/main/java/at/ac/tuwien/dsg/hinc/softwareartefactservice/repository/InteroperabilityBridgeRepository.java
@@ -3,7 +3,7 @@ package at.ac.tuwien.dsg.hinc.softwareartefactservice.repository;
 import org.springframework.stereotype.Repository;
 import sinc.hinc.common.model.InteroperabilityBridge;
 import sinc.hinc.common.model.SoftwareArtefact;
-import sinc.hinc.repository.DAO.orientDB.AbstractDAO;
+import sinc.hinc.repository.DAO.AbstractDAO;
 
 @Repository
 public class InteroperabilityBridgeRepository extends AbstractDAO<InteroperabilityBridge> {
--- software-artefact-service/src/main/java/at/ac/tuwien/dsg/hinc/softwareartefactservice/repository/SoftwareArtefactRepository.java
+++ software-artefact-service/src/main/java/at/ac/tuwien/dsg/hinc/softwareartefactservice/repository/SoftwareArtefactRepository.java
@@ -2,7 +2,7 @@ package at.ac.tuwien.dsg.hinc.softwareartefactservice.repository;
 
 import org.springframework.stereotype.Repository;
 import sinc.hinc.common.model.SoftwareArtefact;
-import sinc.hinc.repository.DAO.orientDB.AbstractDAO;
+import sinc.hinc.repository.DAO.AbstractDAO;
 
 @Repository
 public class SoftwareArtefactRepository extends AbstractDAO<SoftwareArtefact> {
